d091b985f89491a996343a37e19ff9a11f373291
kapsel <wojtekd2000@gmail.com>
2024-04-21 22:34:40 +0200

    GTI-16 layout improvements

--- src/components/auth/AuthDialog.vue
+++ src/components/auth/AuthDialog.vue
@@ -1,38 +1,37 @@
 <template>
   <q-btn icon="person" outline @click="dialogOpen = true" />
-  <q-dialog v-model="dialogOpen" persistent>
-    <q-card class="auth-dialog-container">
-      <q-card-section>
-        <div class="auth-dialog-inputs">
-            <template v-if="isRegistering">
+  <q-dialog v-model="dialogOpen" persistent class="auth-dialog">
+      <q-card class="auth-dialog-container">
+        <q-card-section class="auth-dialog-title">
+          <span class="text-h6">{{ isRegistering ? $t('auth.register') : $t('auth.login') }}</span>
+        </q-card-section>
+        <q-card-section>
+          <div class="auth-dialog-inputs">
+            <div v-if="isRegistering">
               <q-input v-model="formData.email" :label="$t('auth.email')" type="email"
                 :rules="[validators.required, validators.validateEmail]" />
               <q-input v-model="formData.password" :label="$t('auth.password')" type="password"
                 :rules="[validators.required, validators.minimalPassword, validators.confirmation]" />
               <q-input v-model="formData.repeatPassword" :label="$t('auth.password_confirmation')" type="password"
                 :rules="[validators.required, validators.confirmation]" />
-            </template>
-            <template v-else>
+              <span class="text-subtitle auth-dialog-link" style="margin-right: 5px;" @click="toggleRegister">{{ $t('auth.login_action') }}</span>
+            </div>
+            <div v-else>
               <q-input v-model="formData.email" :label="$t('auth.email')" type="email"
                 :rules="[validators.required]" />
               <q-input v-model="formData.password" :label="$t('auth.password')" type="password"
                 :rules="[validators.required]" />
-              <span class="text-subtitle auth-dialog-link">{{ $t('auth.forgot_password') }}</span><br>
-            </template>
-        </div>
-      </q-card-section>
-      <q-card-actions class="auth-dialog-actions" align="between">
-        <q-btn :label="$t('cancel')" color="negative" @click="cleanDialog" />
-        <div v-if="isRegistering">
-          <q-btn :label="$t('auth.login')" color="secondary" style="margin-right: 5px;" @click="toggleRegister" />
-          <q-btn :label="$t('auth.register')" color="primary" :disable="!isValid" />
-        </div>
-        <div v-else>
-          <q-btn :label="$t('auth.register')" color="secondary" style="margin-right: 5px;" @click="toggleRegister" />
-          <q-btn :label="$t('auth.login')" color="primary" :disable="!isValid" />
-        </div>
-      </q-card-actions>
-    </q-card>
+              <span class="text-subtitle auth-dialog-link">{{ $t('auth.forgot_password') }}</span>
+              <span class="text-subtitle auth-dialog-link" style="margin-right: 5px;" @click="toggleRegister">{{ $t('auth.register_action') }}</span>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-actions class="auth-dialog-actions" align="between">
+          <q-btn :label="$t('cancel')" color="negative" @click="cleanDialog" />
+          <q-btn v-if="isRegistering" :label="$t('auth.register')" color="primary" :disable="!isValid" />
+          <q-btn v-else :label="$t('auth.login')" color="primary" :disable="!isValid" />
+        </q-card-actions>
+      </q-card>
   </q-dialog>
 </template>
 <script setup lang="ts">
@@ -77,8 +76,12 @@ const validators = {
     padding: 10px;
   }
 
+  &-title {
+    margin-bottom: -15px;
+  }
+
   &-link {
-    margin-top: 10px;
+    margin-bottom: 5px;
     display: block;
     cursor: pointer;
     color: $primary;
