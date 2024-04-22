0e4d13d9116ac08126254a856c04f3231789f34e
kapsel <wojtekd2000@gmail.com>
2024-04-23 00:40:11 +0200

    GTI-21 done items list and UX improvements

--- src/components/todo/TodoList.vue
+++ src/components/todo/TodoList.vue
@@ -1,45 +1,104 @@
 <template>
   <div class="q-pa-md">
-    <q-list padding>
-      <q-item class="todo-list-item" tag="label" v-for="(value, index) of options" :key="index">
+    <q-list padding draggable class="todo-list-transition-container">
+      <q-item class="todo-list-item" tag="label" v-for="(value, index) of todoItems" :key="index">
         <q-item-section side top>
-          <q-checkbox v-model="options[index].value" name="optionOne"/>
+          <q-checkbox v-model="todoItems[index].value" name="optionOne"/>
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ value.label }}</q-item-label>
         </q-item-section>
       </q-item>
     </q-list>
+    <q-expansion-item class="bg-info rounded-borders todo-list-transition-container"
+      label="Done"
+      v-model="expandableOpen" :disable="expandableDisabled">
+      <q-list padding>
+        <q-item class="todo-list-item" tag="label"
+          v-for="(value, index) of doneItems" :key="index">
+          <q-item-section side top>
+            <q-checkbox v-model="doneItems[index].value" name="optionOne"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ value.label }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-expansion-item>
   </div>
 </template>
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { computed, reactive, ref, watch } from 'vue';
 
-const options = reactive<{label: string, value: boolean}[]>([
+const items = reactive<{label: string, value: boolean, position: number}[]>([
   {
     label: 'Option 1',
-    value: false
+    value: false,
+    position: 1
   },
   {
     label: 'Option 2',
-    value: false
+    value: false,
+    position: 2
   },
   {
     label: 'Option 3',
-    value: false
+    value: false,
+    position: 3
   },
   {
     label: 'Option 4',
-    value: false
+    value: false,
+    position: 4
   },
   {
     label: 'Option 5',
-    value: false
+    value: false,
+    position: 5
   },
   {
     label: 'Option 6',
-    value: false
+    value: false,
+    position: 6
   }
 ]);
+const todoItems = computed(() => items
+  .filter(item => !item.value)
+  .sort((a, b) => a.position - b.position));
+const doneItems = computed(() => items
+  .filter(item => item.value)
+  .sort((a, b) => a.position - b.position));
+
+const expandableOpen = ref(false);
+const expandableDisabled = ref(doneItems.value.length === 0);
+watch(doneItems, (value, oldValue) => {
+  if (value.length === 0) {
+    expandableOpen.value = false;
+    expandableDisabled.value = true;
+  } else {
+    expandableDisabled.value = false;
+    if (todoItems.value.length === 0 || oldValue.length === 0) {
+      expandableOpen.value = true;
+    }
+  }
+});
+
 </script>
-false, true, false, true
+<style scoped lang="scss">
+.todo-enter-active,
+.todo-leave-active,
+.done-enter-active,
+.done-leave-active {
+  transition: transform 0.5s ease-in;
+}
+
+.todo-enter-from,
+.todo-leave-to {
+  transform: translateX(200%) translateY(200%);
+}
+
+.done-enter-from,
+.done-leave-to {
+  transform: translateX(-200%) translateY(200%);
+}
+</style>
