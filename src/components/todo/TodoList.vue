6d021ba71c37093914b795dbbce8d1b52c8f8408
kapsel <wojtekd2000@gmail.com>
2024-04-22 22:27:48 +0200

    GTI-21 basic list generation

---
+++ src/components/todo/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="q-pa-md">
+    <q-list padding>
+      <q-item class="todo-list-item" tag="label" v-for="(value, index) of options" :key="index">
+        <q-item-section side top>
+          <q-checkbox v-model="options[index].value" name="optionOne"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ value.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const options = reactive<{label: string, value: boolean}[]>([
+  {
+    label: 'Option 1',
+    value: false
+  },
+  {
+    label: 'Option 2',
+    value: false
+  },
+  {
+    label: 'Option 3',
+    value: false
+  },
+  {
+    label: 'Option 4',
+    value: false
+  },
+  {
+    label: 'Option 5',
+    value: false
+  },
+  {
+    label: 'Option 6',
+    value: false
+  }
+]);
+</script>
+false, true, false, true
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,45 +1,10 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page>
     well, nothing's here for now
+    <TodoList/>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { Todo, Meta } from 'components/models';
-
-export default defineComponent({
-  name: 'IndexPage',
-
-  data () {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ];
-
-    const meta: Meta = {
-      totalCount: 1200
-    };
-
-    return { todos, meta };
-  }
-});
+<script setup lang="ts">
+import TodoList from 'components/todo/TodoList.vue';
 </script>
